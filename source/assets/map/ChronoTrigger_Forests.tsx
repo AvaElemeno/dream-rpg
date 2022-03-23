0a4617cde3db1d23aa87e62deb5fe860a817941f
avaelemeno <alexavaleryftw@gmail.com>
2022-03-22 20:42:33 -0400

    update all the nothing

---
+++ source/assets/map/ChronoTrigger_Forests.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="ChronoTrigger_Forests" tilewidth="16" tileheight="16" tilecount="3484" columns="52">
+ <image source="ChronoTrigger_Forests.png" width="832" height="1072"/>
+</tileset>
---
+++ source/assets/map/FireRed_tileset_2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.7.2" name="FireRed_tileset_2" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="1316" columns="28">
+ <image source="FireRed_tileset_2.png" width="477" height="800"/>
+</tileset>
